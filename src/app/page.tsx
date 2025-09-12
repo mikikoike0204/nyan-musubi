07a6428a3a9e98234a8da1efa07a010e756750a0
mikikoike <m.koike@triarch.co.jp>
2025-09-12 17:55:32 +0900

    コンポーネント化途中まで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import TopSlider from "@/components/TopSlider/TopSlider";
 import Image from "next/image";
 import Link from "next/link";
 import "./style.css"
+import CatList from "@/components/CatList/CatList";
 
 export default function Home() {
   return (
@@ -34,218 +35,8 @@ export default function Home() {
           <div className="c-section-title-wrap">
             <h2 className="c-section-title">新着のねこちゃん</h2>
           </div>
-          <ul className="p-top-newcat__list">
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/top/newcat-list-img01.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    12ヶ月 / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">茶白</div>
-                    <h3 className="p-top-newcat__text2-title">
-                      人懐っこい男の子
-                    </h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/sample/newcat-list-img02.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    6ヶ月 / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">キジトラ</div>
-                    <h3 className="p-top-newcat__text2-title">元気な女の子</h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/sample/newcat-list-img03.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    3ヶ月 / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">キジ白</div>
-                    <h3 className="p-top-newcat__text2-title">
-                      おとなしい男の子
-                    </h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/sample/newcat-list-img04.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    シニア / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">サバトラ</div>
-                    <h3 className="p-top-newcat__text2-title">
-                      落ち着きのある男の子
-                    </h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/top/newcat-list-img01.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    12ヶ月 / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">茶白</div>
-                    <h3 className="p-top-newcat__text2-title">
-                      人懐っこい男の子
-                    </h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/sample/newcat-list-img02.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    6ヶ月 / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">キジトラ</div>
-                    <h3 className="p-top-newcat__text2-title">元気な女の子</h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/sample/newcat-list-img03.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    3ヶ月 / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">キジ白</div>
-                    <h3 className="p-top-newcat__text2-title">
-                      おとなしい男の子
-                    </h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-            <li className="p-top-newcat__item">
-              <a className="p-top-newcat__link" href="">
-                <div className="p-top-newcat__image">
-                  <Image
-                    src="/sample/newcat-list-img04.jpg"
-                    alt="新着のねこちゃん画像"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    className="p-top-newcat__image-pic"
-                  />
-                </div>
-                <div className="p-top-newcat__desc">
-                  <div className="p-top-newcat__text1">
-                    ミックス ♂<br />
-                    シニア / 関東
-                  </div>
-                  <div className="p-top-newcat__text2">
-                    <div className="p-top-newcat__text2-s">サバトラ</div>
-                    <h3 className="p-top-newcat__text2-title">
-                      落ち着きのある男の子
-                    </h3>
-                  </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
-                </div>
-              </a>
-            </li>
-          </ul>
+          <CatList />
 
-          <div className="p-top-newcat__more">
-            <Link href="/cats" className="c-common-btn">
-              もっと見る
-            </Link>
-          </div>
         </div>
       </section>
 
@@ -285,7 +76,7 @@ export default function Home() {
                       人懐っこい男の子
                     </h3>
                   </div>
-                  <div className="block py-2 px-1 bg-[#4a90e2] text-white text-sm mt-5 cursor-pointer text-center mt-5">
+                  <div className="p-top-newcat__adopted-label">
                     家族が決まりました！
                   </div>
                 </div>
@@ -319,7 +110,9 @@ export default function Home() {
                     <div className="p-top-newcat__text2-s">キジトラ</div>
                     <h3 className="p-top-newcat__text2-title">元気な女の子</h3>
                   </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
+                  <div className="p-top-newcat__adopted-label">
+                    家族が決まりました！
+                  </div>
                 </div>
               </a>
             </li>
@@ -353,7 +146,9 @@ export default function Home() {
                       おとなしい男の子
                     </h3>
                   </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
+                  <div className="p-top-newcat__adopted-label">
+                    家族が決まりました！
+                  </div>  
                 </div>
               </a>
             </li>
@@ -387,7 +182,9 @@ export default function Home() {
                       落ち着きのある男の子
                     </h3>
                   </div>
-                  <button className="p-top-newcat__fav">お気に入り❤︎</button>
+                  <div className="p-top-newcat__adopted-label">
+                    家族が決まりました！
+                  </div>
                 </div>
               </a>
             </li>
---
+++ src/components/CatList/CatList.tsx
@@ -0,0 +1,224 @@
+// src/components/CatList/CatList.tsx
+import React from "react";
+import "./style.css";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function CatList() {
+  return (
+    <>
+      <ul className="p-top-newcat__list">
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/top/newcat-list-img01.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                12ヶ月 / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">茶白</div>
+                <h3 className="p-top-newcat__text2-title">
+                  人懐っこい男の子
+                </h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/sample/newcat-list-img02.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                6ヶ月 / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">キジトラ</div>
+                <h3 className="p-top-newcat__text2-title">元気な女の子</h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/sample/newcat-list-img03.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                3ヶ月 / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">キジ白</div>
+                <h3 className="p-top-newcat__text2-title">
+                  おとなしい男の子
+                </h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/sample/newcat-list-img04.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                シニア / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">サバトラ</div>
+                <h3 className="p-top-newcat__text2-title">
+                  落ち着きのある男の子
+                </h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/top/newcat-list-img01.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                12ヶ月 / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">茶白</div>
+                <h3 className="p-top-newcat__text2-title">
+                  人懐っこい男の子
+                </h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/sample/newcat-list-img02.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                6ヶ月 / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">キジトラ</div>
+                <h3 className="p-top-newcat__text2-title">元気な女の子</h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/sample/newcat-list-img03.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                3ヶ月 / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">キジ白</div>
+                <h3 className="p-top-newcat__text2-title">
+                  おとなしい男の子
+                </h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+        <li className="p-top-newcat__item">
+          <Link className="p-top-newcat__link" href="">
+            <div className="p-top-newcat__image">
+              <Image
+                src="/sample/newcat-list-img04.jpg"
+                alt="新着のねこちゃん画像"
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-top-newcat__image-pic"
+              />
+            </div>
+            <div className="p-top-newcat__desc">
+              <div className="p-top-newcat__text1">
+                ミックス ♂<br />
+                シニア / 関東
+              </div>
+              <div className="p-top-newcat__text2">
+                <div className="p-top-newcat__text2-s">サバトラ</div>
+                <h3 className="p-top-newcat__text2-title">
+                  落ち着きのある男の子
+                </h3>
+              </div>
+              <button className="p-top-newcat__fav">お気に入り❤︎</button>
+            </div>
+          </Link>
+        </li>
+      </ul>
+      <div className="p-top-newcat__more">
+        <Link href="/cats" className="c-common-btn">
+          もっと見る
+        </Link>
+      </div>
+    </>
+  );
+}
+
