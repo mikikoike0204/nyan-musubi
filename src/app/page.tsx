ea47023d0489cd3cfc9f95574689a7bf90163aad
mikikoike <mikikoike1@gmail.com>
2025-10-16 02:29:56 +0900

    topのcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import TopSlider from "@/components/TopSlider/TopSlider";
 import Link from "next/link";
-import "./style.css";
+import styles from "./page.module.css";
 import CatList from "@/components/CatList/CatList";
 
 export default function Home() {
@@ -10,18 +10,18 @@ export default function Home() {
       <section className="c-section p-top-mv">
         <div className="c-container">
           <TopSlider />
-          <div className="p-top-mv__desc">
-            <p className="p-top-mv__text">
+          <div className={styles.topMvDesc}>
+            <p className={styles.topMvText}>
               ねこを飼った事がないけれど、
               <br />
               困っている子を家族としてお迎えしたい。
             </p>
-            <p className="p-top-mv__text">
+            <p className={styles.topMvText}>
               ねこについて詳しくないけど、
               <br />
               ねこを保護したので家族になってくれる人を探したい。
             </p>
-            <p className="p-top-mv__text">
+            <p className={styles.topMvText}>
               そんな優しい人たちの間で、にゃんこのご縁を結びたい。
               <br />
               そう思って出来たサイト「にゃん結び」です。
@@ -30,7 +30,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="c-section p-top-newcat">
+      <section className={`c-section ${styles.topNewCat}`}>
         <div className="c-container">
           <div className="c-section-title-wrap">
             <h2 className="c-section-title">新着のねこちゃん</h2>
@@ -44,7 +44,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="c-section p-top-adopted">
+      <section className="c-section">
         <div className="c-container">
           <div className="c-section-title-wrap">
             <h2 className="c-section-title">家族が決定したねこちゃん</h2>
